276dde3cc7c4648d11f33217ce337e82726dea34
Xara Mourgela <charalampiamourgela@gmail.com>
2025-01-14 23:53:07 +0200

    Footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,41 +1,36 @@
 import { Component, OnInit } from '@angular/core';
-import { Router, RouterOutlet } from '@angular/router';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { AdminHomeComponent } from "./pages/admin/admin-home/admin-home.component";
 import { LoginComponent } from "./pages/login/login.component";
 import { NgIf } from '@angular/common';
 import { NavbarComponent } from "./shared/navbar/navbar.component";
 import { AuthService } from './services/auth/auth.service';
+import { FooterComponent } from "./shared/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AdminHomeComponent, LoginComponent, NgIf, NavbarComponent],
+  imports: [RouterOutlet, AdminHomeComponent, LoginComponent, NgIf, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   title = 'Technico';
+  showFooter = true;
+
+  constructor(private router: Router, private authService: AuthService) {}
 
 
-	constructor(private router: Router, private authService: AuthService ) {}
-		
-
-		ngOnInit(): void {
-			
-		
-	// 	if (this.authService.isTokenPresent()) {
-	// 		const userType = this.authService.getUserType(); // Get userType from token
-	  
-	// 		// Redirect based on the userType
-	// 		if (userType === 'Admin') {
-	// 		  this.router.navigate(['/admin-home']);
-	// 		} else if (userType === 'User') {
-	// 		  this.router.navigate(['/user-home']);
-	// 		}
-	// 	  } else {
-	// 		// If no token is present, redirect to login
-	// 		this.router.navigate(['/login']);
-	// }
-
-	
-}}
+	  currentRoute: string = '';
+	  hiddenNavbarRoutes: string[] = ['/login', '/signup']; // Navbar δε θα εμφανίζεται εδώ
+	  hiddenFooterRoutes: string[] = ['/login', '/signup', '/create-repair']; // Footer δε θα εμφανίζεται εδώ
+    
+	  ngOnInit(): void {
+		// Ακούμε για αλλαγές στη διαδρομή
+		this.router.events.subscribe(event => {
+		  if (event instanceof NavigationEnd) {
+			this.currentRoute = event.urlAfterRedirects;
+		  }
+		});
+  }
+}
--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -8,5 +8,6 @@ import { Component } from '@angular/core';
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
+  email: string = 'technico@company.com';
 
 }
